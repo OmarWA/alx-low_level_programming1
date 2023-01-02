5b4aff4e83b885ca721b6ab26a0ce3807e84e442
OmarWael <82493525+OmarWaelAhmed@users.noreply.github.com>
2023-01-02 11:38:11 -0800

    Pushing emacs configuration file for Betty

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point to the whole program
+ * Return:
+ * 0 if everything went okay, non-zero otherwise
+ */
+int main(void)
+{
+	return ("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+}
