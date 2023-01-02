2e6adc94199541091b42c84106f64bc66ae51e0f
OmarWael <82493525+OmarWaelAhmed@users.noreply.github.com>
2023-01-01 17:47:09 -0800

    Fixing code style and documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main (void)
+/**
+ * main -- This is the entry point of the whole program
+ *
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
